1608436d43175ccf6470bcacc55abf038c80eda1
Alejo Boga <alejoboga20@gmail.com>
2021-07-13 14:41:45 -0300

    handling form inputs

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Question = () => {
+  const [budget, setBudget] = useState(0);
+
+  const handleSetBudget = (e) => {
+    setBudget(parseInt(e.target.value));
+  };
+
+  const handleAddBudget = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <h2>Put your Budget</h2>
 
-      <form>
-        <input type='number' className='u-full-width' placeholder='budget' />
+      <form onSubmit={handleAddBudget}>
+        <input
+          type='number'
+          className='u-full-width'
+          placeholder='budget'
+          onChange={handleSetBudget}
+          value={budget}
+        />
         <input
           type='submit'
           className='button-primary u-full-width'
