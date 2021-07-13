0310e3b9ec40386647210023cee6213a31678854
Alejo Boga <alejoboga20@gmail.com>
2021-07-13 15:24:33 -0300

    Handling inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
 
 export const Form = () => {
+  const [expense, setExpense] = useState('');
+  const [quantity, setQuantity] = useState(0);
+
+  const handleAddExpense = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleAddExpense}>
       <h2>Add Expenses</h2>
 
       <div className='campo'>
@@ -11,10 +18,17 @@ export const Form = () => {
           type='text'
           className='u-full-width'
           placeholder='Expense Description'
+          value={expense}
+          onChange={(e) => setExpense(e.target.value)}
         />
 
         <label>Quantity: </label>
-        <input type='number' className='u-full-width' />
+        <input
+          type='number'
+          className='u-full-width'
+          value={quantity}
+          onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
+        />
 
         <input
           type='submit'
