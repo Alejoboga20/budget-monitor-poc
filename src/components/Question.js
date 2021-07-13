42bcaeae91246f65994d2986b8a70b24d3b1b526
Alejo Boga <alejoboga20@gmail.com>
2021-07-13 14:47:14 -0300

    Handling error msg

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { Error } from './Error';
+
+const msg = 'Invalid Budget. Please enter a valid value.';
 
 export const Question = () => {
   const [budget, setBudget] = useState(0);
+  const [error, setError] = useState(false);
 
   const handleSetBudget = (e) => {
     setBudget(parseInt(e.target.value));
@@ -9,11 +13,20 @@ export const Question = () => {
 
   const handleAddBudget = (e) => {
     e.preventDefault();
+
+    if (budget < 1 || isNaN(budget)) {
+      setError(true);
+      return;
+    }
+
+    setError(false);
   };
 
   return (
     <>
       <h2>Put your Budget</h2>
+
+      {error ? <Error msg={msg} /> : null}
 
       <form onSubmit={handleAddBudget}>
         <input
