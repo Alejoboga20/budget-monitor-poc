48f3d16de7b8117fc4e5107a12e7905adf77af78
Alejo Boga <alejoboga20@gmail.com>
2021-07-13 15:30:10 -0300

    Validating inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,27 @@
 import React, { useState } from 'react';
+import { Error } from './Error';
+
+const msg = 'Both fields are mandatory and should be valid values';
 
 export const Form = () => {
   const [expense, setExpense] = useState('');
   const [quantity, setQuantity] = useState(0);
+  const [error, setError] = useState(false);
 
   const handleAddExpense = (e) => {
     e.preventDefault();
+
+    if (quantity < 1 || isNaN(quantity) || expense.trim() === '') {
+      setError(true);
+      return;
+    }
   };
 
   return (
     <form onSubmit={handleAddExpense}>
       <h2>Add Expenses</h2>
+
+      {error && <Error msg={msg} />}
 
       <div className='campo'>
         <label>Expense: </label>
