f55ea46b1a8d608bb4137d2e1390c54bb0631a42
Alejo Boga <alejoboga20@gmail.com>
2021-07-16 12:20:45 -0300

    Refactor

--- src/App.js
+++ src/App.js
@@ -19,33 +19,30 @@ export const App = () => {
     saveExpense,
   ] = useApp();
 
+  const componentToRender = showQuestion ? (
+    <Question
+      setBudget={setBudget}
+      setRest={setRest}
+      setShowQuestion={setShowQuestion}
+    />
+  ) : (
+    <div className='row'>
+      <div className='one-half column'>
+        <Form saveExpense={saveExpense} setCreateExpense={setCreateExpense} />
+      </div>
+
+      <div className='one-half column'>
+        <List expenses={expenses} />
+        <ExpenseControl budget={budget} rest={rest} />
+      </div>
+    </div>
+  );
+
   return (
     <div className='container'>
       <Header headerTitle='Weekly Expenses' />
 
-      <div className='contenido-principal contenido'>
-        {showQuestion ? (
-          <Question
-            setBudget={setBudget}
-            setRest={setRest}
-            setShowQuestion={setShowQuestion}
-          />
-        ) : (
-          <div className='row'>
-            <div className='one-half column'>
-              <Form
-                saveExpense={saveExpense}
-                setCreateExpense={setCreateExpense}
-              />
-            </div>
-
-            <div className='one-half column'>
-              <List expenses={expenses} />
-              <ExpenseControl budget={budget} rest={rest} />
-            </div>
-          </div>
-        )}
-      </div>
+      <div className='main-content content'>{componentToRender}</div>
     </div>
   );
 };
